11f9474c63e065df1f0d03cf1b162fde81b0dd85
unknown <omarkhaledvii@gmail.com>
2019-05-02 02:06:24 +0200

    Fix login UI

--- Trivago/Forms/Login.cs
+++ Trivago/Forms/Login.cs
@@ -26,7 +26,7 @@ namespace Trivago.Forms
            );
         public static string userID = "1";
         OracleConnection conn;
-        string connST = "Data Source=ORCL;User Id=HR;Password=ALAAalaa21;";
+        string connST = "Data Source=ORCL;User Id=HR;Password=hr;";
         public Login()
         {
             InitializeComponent();
